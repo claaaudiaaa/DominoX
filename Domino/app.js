af796998b149341eb11c17cc637c656fbfe97060
claudia <cld.apopei@yahoo.com>
2016-02-05 11:36:25 +0200

    aa

---
+++ Domino/app.js
@@ -0,0 +1,49 @@
+(function () {
+
+    var canvas = document.getElementById("canv");
+    var gl = canvas.getContext("experimental-webgl") || canvas.getContext("webgl") || canvas.getContext("moz-webgl") || canvas.getContext("webkit-3d");
+    var context = canvas.getContext("2d");
+    var img = new Image();
+    
+    img.src = "../images/tile6-6.png";
+    console.log("creating webgl");
+    console.log(img);
+    context.drawImage(img, 10, 10);
+    
+    //cubeTexture = context.createTexture();
+    
+    //cubeImage = new Image();
+    //cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); }
+    
+    //cubeImage.src = "../images/tile6-6.png";
+
+    if (gl) {
+
+        var extensions = gl.getSupportedExtensions();
+        console.log(gl);
+        console.log(extensions);
+            
+    }
+    else {
+
+        console.log("Browser does not support webgl");
+    }
+    
+})();
+
+
+
+function handleTextureLoaded(image, texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+}
+ 
+function draw() {
+
+    
+}
---
+++ Domino/render.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    console.log("adding jquery");
+    var newscript = document.createElement('script');
+    newscript.type = 'text/javascript';
+    newscript.async = true;
+    newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
+
+
+function play() {
+    var playBtn = document.getElementById('play');
+    document.getElementById('mainMenu').style.visibility = "hidden";
+    document.getElementById('newGame').style.visibility = "visible";
+};
+
+
+function restore() {
+    var resumeBtn = document.getElementById('resume');
+    document.getElementById('resumeGame').style.visibility = "visible";
+    document.getElementById('mainMenu').style.visibility = "hidden";
+};
+
+
+function viewScores() {
+    var scoresBtn = document.getElementById('viewScores');
+    document.getElementById('scores').style.visibility = "visible";
+    document.getElementById('mainMenu').style.visibility = "hidden";
+};
+
+function goToInsertPlayers() {
+    document.getElementById('insertPlayers').style.visibility = "visible";
+    document.getElementById('newGame').style.visibility = "hidden";
+}
+
+function goToMainMenu() {
+    document.getElementById('mainMenu').style.visibility = "visible";
+    document.getElementById('scores').style.visibility = "hidden";
+    document.getElementById('newGame').style.visibility = "hidden";
+    document.getElementById('resumeGame').style.visibility = "hidden";
+    document.getElementById('insertPlayers').style.visibility = "hidden";
+}
+
+function startGame() {
+    document.getElementById('mainMenu').style.visibility = "hidden";
+    document.getElementById('scores').style.visibility = "hidden";
+    document.getElementById('newGame').style.visibility = "hidden";
+    document.getElementById('resumeGame').style.visibility = "hidden";
+    document.getElementById('insertPlayers').style.visibility = "hidden";
+    $('.backgroundImage').load("gamePage.html");
+}
