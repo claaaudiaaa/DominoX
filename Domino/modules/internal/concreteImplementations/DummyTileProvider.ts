5fbb79fa02bd83b9327cb0abb1740f8c61b7f25f
claudia <cld.apopei@yahoo.com>
2016-02-06 18:20:49 +0200

    minor

--- Domino/modules/internal/concreteImplementations/DummyTileProvider.ts
+++ Domino/modules/internal/concreteImplementations/DummyTileProvider.ts
@@ -13,17 +13,29 @@ module dominox {
             return tileList;
 
         }
-        getRandomTile(): dominox.DominoTile
-        {
+        getRandomTile(): dominox.DominoTile {
             var allTiles: dominox.DominoTile[] = this.createAllDominoTiles();
             var randomNo: number = dominox.randomIntFromInterval(0, 28);
             var tile: dominox.DominoTile = allTiles[randomNo];
             delete allTiles[randomNo];
 
-            var bone: dominox.DominoBone = new dominox.DominoBone(first, second);
-            var tile: dominox.DominoTile = new dominox.DominoTile(bone, dominox.DominoTileOrientation.HorizontalFirstLeft);
-
             return tile;
+        }
+        createAllDominoTiles(): dominox.DominoTile[] {
+            var allTiles: dominox.DominoTile[];
+            for (var i = 0; i < 7; i++) {
+                for (var j = i; j < 7; j++) {
+                    var bone: dominox.DominoBone = new dominox.DominoBone(i, j);
+                    var tile: dominox.DominoTile = new dominox.DominoTile(bone, dominox.DominoTileOrientation.HorizontalFirstLeftSecondRight);
+                    console.log(tile);
+                    allTiles.push(tile);
+                }
+            }
+            
+            for (var k = 0; k < 28; k++) {
+                console.log(allTiles[k]);
+            }
+            return allTiles;
         }
     }
 }
